d3cb36d5aa3b9c8f9bf005517957ab2532c643de
roytravel <roytravel97@gmail.com>
2020-02-15 04:09:04 +0900

    Edit(Dynamic Programming): fibonacci sequence

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -5,7 +5,14 @@ using namespace std;
 long long array[SIZE] = { 0, 1, 1, };
 
 long long fibonacci(int num) 
-{ 
+{
+	
+	if (num == 0)
+		return 0;
+
+	if (num == 1 || num == 2)
+		return 1;
+
 	if (array[num])
 		return array[num];
 
