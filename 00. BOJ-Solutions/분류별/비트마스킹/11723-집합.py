6c9f4b5603a7eb4f4157d23a0170c6fb7e84deb5
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-01-26 03:01:00 +0900

    Add problem solving code

---
+++ 00. BOJ-Solutions/분류별/비트마스킹/11723-집합.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+M = int(input())
+S = set()
+
+for _ in range(M):
+    cmd = input().split()
+    if cmd[0] == "all":
+        S = set([i for i in range(1, 21)])
+    elif cmd[0] == "empty":
+        S = set()
+    elif cmd[0] == "add":
+        S.add(int(cmd[1]))
+    elif cmd[0] == "check":
+        print (1 if int(cmd[1]) in S else 0)
+    elif cmd[0] == "remove":
+        S.discard(int(cmd[1]))
+    elif cmd[0] == "toggle":
+        if int(cmd[1]) in S:
+            S.discard(int(cmd[1]))
+        else:
+            S.add(int(cmd[1]))
