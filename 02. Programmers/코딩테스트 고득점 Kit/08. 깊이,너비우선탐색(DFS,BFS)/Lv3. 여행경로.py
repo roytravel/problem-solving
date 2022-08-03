bd03ae1c897f7ca5ed3d2da3bfaa71de0cb47dcf
roytravel <roytravel97@gmail.com>
2022-08-04 03:05:09 +0900

    Add dp problem solution

--- 02. Programmers/코딩테스트 고득점 Kit/08. 깊이,너비우선탐색(DFS,BFS)/Lv3. 여행경로.py
+++ 02. Programmers/코딩테스트 고득점 Kit/08. 깊이,너비우선탐색(DFS,BFS)/Lv3. 여행경로.py
@@ -1,22 +1,47 @@
 from collections import defaultdict
 
-answer = []
-graph = defaultdict(list)
-
-def dfs(src):
-    while graph[src]:
-        dfs(graph[src].pop(0))
-        
-    if not graph[src]:
-        answer.append(src)
-        return
-
-def solution(tickets):    
+def solution(tickets):
+    graph = defaultdict(list)
+    answer = []
     for src, dst in tickets:
         graph[src].append(dst)
     
-    for k, v in graph.items():
-        graph[k].sort()
+    for src, dst in graph.items():
+        graph[src].sort(reverse=True)
+
+    stack = ['ICN']
+    while stack:
+        src = stack[-1]
+        if not graph[src]:
+            answer.append(stack.pop())
+        else:
+            stack.append(graph[src].pop())
+    answer.reverse()
+    return answer
+
+
+# from collections import defaultdict
+
+# answer = []
+# graph = defaultdict(list)
+
+# def dfs(src):
+#     while graph[src]:
+#         dfs(graph[src].pop(0))
         
-    dfs("ICN")
-    return answer[::-1]
+#     if not graph[src]:
+#         answer.append(src)
+#         return
+
+# def solution(tickets):    
+#     for src, dst in tickets:
+#         graph[src].append(dst)
+    
+#     for k, v in graph.items():
+#         graph[k].sort()
+        
+#     dfs("ICN")
+#     return answer[::-1]
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+#print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
