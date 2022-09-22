cab5fae83b7e1392ba361c7ada9afc2981552dec
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-09-23 01:27:04 +0900

    Add shortest path algortihms in graph

--- 99. others/Data Structure/탐색 알고리즘/dijkstra.py
+++ 99. others/Data Structure/탐색 알고리즘/dijkstra.py
@@ -1,38 +1,45 @@
+""" This is input value. you can just copy and paste it. 
+5 6 1
+1 2 3
+1 4 8
+1 5 9
+2 1 2
+2 3 2
+3 2 2
+3 4 1
+3 5 3
+4 1 8
+4 3 1
+5 1 9
+"""
+
 import heapq
 
 def dijkstra(start):
     heap = []
-    heapq.heappush(heap, [0, start])
+    heapq.heappush(heap, [0, start]) # 최소힙은 배열의 첫 번째 값을 기준으로 배열을 정렬.
     INF = float('inf')
-    weights = [INF] * (vertex+1)
-    weights[start] = 0
+    weights = [INF] * (vertex+1) # DP에 활용할 memoization 테이블 생성
+    weights[start] = 0 # 자기 자신으로 가는 사이클은 없으므로.
     
     while heap:
         weight, node = heapq.heappop(heap)
-        if weight > weights[node]:
+        if weight > weights[node]: # 비용 최적화 전부터 큰 비용일 경우 고려할 필요 없음.
             continue
         
-        for n, w in graph[node]:
+        for n, w in graph[node]: # 최소 비용을 가진 노드를 그리디하게 방문한 경우 연결된 간선 모두 확인
             W = weight + w
-            if weights[n] > W:
-                weights[n] = W
-                heapq.heappush(heap, (W, n))
+            if weights[n] > W: # 여러 경로를 방문해 합쳐진 가중치 W가 더 비용이 적다면 
+                weights[n] = W # 업데이트
+                heapq.heappush(heap, (W, n)) # 최소 비용을 가진 노드와 합쳐진 가중치 추가
 
     return weights
 
-vertex, edge, start = 5, 6, 1 # 노드 개수, 간선, 시작점
+vertex, edge, start = map(int, input().split())
 graph = [[] for _ in range(vertex+1)]
-graph[1].append([2, 3])
-graph[1].append([4, 8])
-graph[1].append([5, 9])
-graph[2].append([1, 2])
-graph[2].append([3, 2])
-graph[3].append([2, 2])
-graph[3].append([4, 1])
-graph[3].append([5, 3])
-graph[4].append([1, 8])
-graph[4].append([3, 1])
-graph[5].append([1, 9])
+for i in range(vertex+edge):
+    src, dst, weight = map(int, input().split())
+    graph[src].append([dst, weight])
 
 weights = dijkstra(start)
-print (weights)
+print (weights) # [inf, 0, 3, 5, 6, 8]
