3dc6f984b3c0723230853d6418f7ab92fb205ed0
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-09-23 16:03:06 +0900

    Add algorithm solutions

--- 00. BOJ-Solutions/브루트포스/15686-치킨 배달.py
+++ 00. BOJ-Solutions/브루트포스/15686-치킨 배달.py
@@ -1,37 +1,72 @@
 import sys
 from itertools import combinations
-input = sys.stdin.readline
 
-N, M = list(map(int, input().split()))
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-city_matrix = []
+def get_chicken_pos() -> list:
+    pos = []
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == 2:
+                pos.append([i+1, j+1])
+    return pos
 
-for _ in range(N):
-    city_matrix.append(list(map(int, input().split())))
-
-houses = []
-chickens = []
-chicken_dists = []
-
-for i in range(N):
-    for j in range(N):
-        if city_matrix[i][j] == 1:
-            houses.append([i+1, j+1])
-        
-        if city_matrix[i][j] == 2:
-            chickens.append([i+1, j+1])
-
-
-chicken = list(combinations(chickens, M))
-
-for i in range(len(chicken[0])):
-    print (chicken[0][i])
-    dist = []
-    for j in range(len(houses)):
-        dist.append(abs(chicken[0][i][0] - houses[j][0]) + abs(chicken[0][i][1] - houses[j][1]))
-        print (dist)
-    chicken_dists.append(min(dist))
-    print (chicken_dists)
+def remove_chicken_house(pos: list) -> int:
+    distance = []    
+    combination = combinations(pos, M)
+    for comb in list(combination):
+        dists = 0
+        for i in range(N):
+            for j in range(N):
+                if graph[i][j] == 1:
+                    dist = sys.maxsize
+                    for c in comb:
+                        dist = min(dist, (abs(c[0] - (i+1)) + abs(c[1] - (j+1))))
+                    dists += dist
+        distance.append(dists)
+    return min(distance)
     
-print (chicken_dists)
-print (sum(chicken_dists))
+if __name__ == "__main__":
+    pos = get_chicken_pos()
+    dist = remove_chicken_house(pos)
+    print (dist)
+
+# Wrong
+# import sys
+# from itertools import combinations
+# input = sys.stdin.readline
+
+# N, M = list(map(int, input().split()))
+
+# city_matrix = []
+
+# for _ in range(N):
+#     city_matrix.append(list(map(int, input().split())))
+
+# houses = []
+# chickens = []
+# chicken_dists = []
+
+# for i in range(N):
+#     for j in range(N):
+#         if city_matrix[i][j] == 1:
+#             houses.append([i+1, j+1])
+        
+#         if city_matrix[i][j] == 2:
+#             chickens.append([i+1, j+1])
+
+
+# chicken = list(combinations(chickens, M))
+
+# for i in range(len(chicken[0])):
+#     print (chicken[0][i])
+#     dist = []
+#     for j in range(len(houses)):
+#         dist.append(abs(chicken[0][i][0] - houses[j][0]) + abs(chicken[0][i][1] - houses[j][1]))
+#         print (dist)
+#     chicken_dists.append(min(dist))
+#     print (chicken_dists)
+    
+# print (chicken_dists)
+# print (sum(chicken_dists))
